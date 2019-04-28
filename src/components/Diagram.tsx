0d245b1990424afb1fecd2f32e294d52a84f2006
MaartenGDev <maarten.grootoonk@gmail.com>
2019-04-28 19:47:55 +0200

    Add domains and datatypes to model

--- src/components/Diagram.tsx
+++ src/components/Diagram.tsx
@@ -1,215 +1,162 @@
-import React, { Component } from 'react';
-import {Connection, Endpoint, jsPlumb} from 'jsplumb';
+import React, {Component} from 'react';
+import {Connection, Endpoint, jsPlumb, jsPlumbInstance} from 'jsplumb';
 import IModel from "../models/IModel";
+import EndpointFactory from "../helpers/EndpointFactory";
 
 interface IProps {
-  model: IModel
+    model: IModel
 }
 
-interface IState {}
+interface IState {
+}
 
 const confirm = (data: any) => {
-  return true;
+    return true;
 };
 
 class Diagram extends Component<IProps, IState> {
-  componentDidMount () {
-    const {model} = this.props;
+    private diagram: any;
 
-    this.loadDiagram(model);
-  }
+    async componentDidMount() {
+        const {model} = this.props;
 
-  buildCardinalityTag (component: any, model: IModel, useFrom: boolean) {
-    const {sourceId, targetId} = component;
-    const relation = model.relations.find(x => x.from.ref === sourceId && x.to.ref === targetId);
-
-    if(relation === undefined){
-      return null;
+        this.diagram = await this.setupDiagram();
+        this.loadDiagram(model);
     }
 
-    const elem = document.createElement('div');
-    elem.classList.add('tag', 'z-10');
-    elem.innerHTML = `<p>${useFrom ? relation.from.cardinality : relation.to.cardinality}</p>`;
-    return elem;
-  }
+    componentWillReceiveProps(nextProps: Readonly<IProps>, nextContext: any): void {
+        this.loadDiagram(nextProps.model);
+    }
 
-  loadDiagram (model: IModel) {
-    const anchorPoints = [
-      [0.2, 0, 0, 0, 0, 0],
-      [0.5, 0, 0, 0, 0, 0],
-      [0.8, 0, 0, 0, 0, 0],
+    setupDiagram(): any {
+        return new Promise((res, rej) => {
+            (jsPlumb as any).ready(() => {
+                res(jsPlumb.getInstance({
+                    DragOptions: {cursor: 'pointer', zIndex: 1},
+                    Container: 'canvas'
+                }));
+            });
+        });
+    }
 
-      [0, 0.2, 0, 0, 0, 0],
-      [0, 0.5, 0, 0, 0, 0],
-      [0, 0.8, 0, 0, 0, 0],
+    loadDiagram(model: IModel) {
+        this.diagram.batch(() => {
+            const connections: { [key: string]: string[] } = model.relations.reduce((acc: { [key: string]: string[] }, cur) => {
+                if (!acc.hasOwnProperty(cur.from.ref)) {
+                    acc[cur.from.ref] = [];
+                }
 
-      [1, 0.2, 0, 0, 0, 0],
-      [1, 0.5, 0, 0, 0, 0],
-      [1, 0.8, 0, 0, 0, 0],
+                acc[cur.from.ref] = [...acc[cur.from.ref], cur.to.ref];
+                return acc;
+            }, {});
 
-      [0.2, 1, 0, 0, 0, 0],
-      [0.5, 1, 0, 0, 0, 0],
-      [0.8, 1, 0, 0, 0, 0]
-    ];
+            const entityElements = document.querySelectorAll('.editor .entity');
 
-    (jsPlumb as any).ready(() => {
-      var lineColor = '#30364c',
-        exampleDropOptions = {
-          tolerance: 'touch',
-          hoverClass: 'dropHover',
-          activeClass: 'dragActive'
-        },
-        connector = ['Bezier', {cssClass: 'connectorClass', hoverClass: 'connectorHoverClass'}],
-        connectorStyle = {
-          strokeWidth: 3,
-          stroke: lineColor
-        },
-        hoverStyle = {
-          stroke: '#449999'
-        },
-        overlays = [
-          ['Custom', {
-            create: (component: any) => this.buildCardinalityTag(component, model, true),
-            location: 0.1,
-            id: 'fromCardinalityOverlay'
-          }],
-          ['Custom', {
-            create: (component: any) => this.buildCardinalityTag(component, model, false),
-            location: 0.9,
-            id: 'toCardinalityOverlay'
-          }]
-        ],
-        endpoint = ['Dot', {cssClass: 'endpointClass', radius: 5, hoverClass: 'endpointHoverClass'}],
-        endpointStyle = {fill: lineColor},
-        anEndpoint = {
-          endpoint: endpoint,
-          paintStyle: endpointStyle,
-          hoverPaintStyle: {fill: '#449999'},
-          isSource: true,
-          isTarget: true,
-          maxConnections: -1,
-          connector: connector,
-          connectorStyle: connectorStyle,
-          connectorHoverStyle: hoverStyle,
-          connectorOverlays: overlays
-        };
+            for (let i = 0; i < entityElements.length; i++) {
+                const sourceId = entityElements[i].id;
 
-      const instance: any = jsPlumb.getInstance({
-        DragOptions: {cursor: 'pointer', zIndex: 1},
-        Container: 'canvas'
-      });
+                if (connections.hasOwnProperty(sourceId)) {
+                    const connected = connections[sourceId];
+                    for (const targetId of connected) {
+                        const sourceEndpoint: Endpoint = this.diagram.addEndpoint(sourceId, EndpointFactory.create(model), {anchor: EndpointFactory.getAnchorPoints()});
+                        const targetEndpoint: Endpoint = this.diagram.addEndpoint(targetId, EndpointFactory.create(model), {anchor: EndpointFactory.getAnchorPoints()});
 
-      instance.batch(() => {
-        const connections: {[key: string]: string[]} = model.relations.reduce((acc: {[key: string]: string[]}, cur) => {
-          if (!acc.hasOwnProperty(cur.from.ref)) {
-            acc[cur.from.ref] = [];
-          }
-
-          acc[cur.from.ref] = [...acc[cur.from.ref], cur.to.ref];
-          return acc;
-        }, {});
-
-        const divsWithWindowClass = document.querySelectorAll('.dynamic-demo .window');
-
-        for (var i = 0; i < divsWithWindowClass.length; i++) {
-          var sourceId = divsWithWindowClass[i].id;
-
-          if (connections.hasOwnProperty(sourceId)) {
-            const connected = connections[sourceId];
-            for (const targetId of connected) {
-              const sourceEndpoint: Endpoint = instance.addEndpoint(sourceId, anEndpoint, {anchor: anchorPoints});
-              const targetEndpoint: Endpoint = instance.addEndpoint(targetId, anEndpoint, {anchor: anchorPoints});
-
-              instance.connect({
-                source: sourceEndpoint,
-                target: targetEndpoint
-              });
+                        this.diagram.connect({
+                            source: sourceEndpoint,
+                            target: targetEndpoint
+                        });
+                    }
+                }
             }
-          }
+
+            this.diagram.bind('click', (conn: Connection) => {
+                this.diagram.detach(conn);
+            });
+
+            this.diagram.bind('beforeDetach', function (conn: Connection) {
+                return confirm('Delete connection?');
+            });
+
+            this.diagram.draggable(entityElements);
+        });
+    }
+
+    calculateScalingFactors(model: IModel) {
+        const diagramWindow = document.querySelector('#diagram-window')!;
+
+        const canvasWidth = diagramWindow.clientWidth;
+        const canvasHeight = diagramWindow.clientHeight;
+
+        const leftX = Math.min(...model.entities.map(entity => entity.location.topLeft.x));
+        const rightX = Math.max(...model.entities.map(entity => entity.location.bottomRight.x));
+
+        const topY = Math.max(...model.entities.map(entity => entity.location.topLeft.y));
+        const bottomY = Math.min(...model.entities.map(entity => entity.location.bottomRight.y));
+
+        const importedWidth = leftX < 0
+            ? (leftX * -1 + rightX)
+            : rightX - leftX;
+
+        const importedHeight = bottomY < 0
+            ? (bottomY * -1 + topY)
+            : topY - bottomY;
+
+        return [canvasWidth / importedWidth, canvasHeight / importedHeight, leftX, topY];
+    }
+
+    calculateLengthBetweenXCoordinates(number: number, leftX: number) {
+        if (number < 0) {
+            return number - leftX;
         }
 
-        // bind click listener; delete connections on click
-        instance.bind('click', function (conn: Connection) {
-          instance.detach(conn);
-        });
-
-        // bind beforeDetach interceptor: will be fired when the click handler above calls detach, and the user
-        // will be prompted to confirm deletion.
-        instance.bind('beforeDetach', function (conn: Connection) {
-          return confirm('Delete connection?');
-        });
-
-        instance.draggable(divsWithWindowClass);
-
-        (jsPlumb as any).fire('jsPlumbDemoLoaded', instance);
-      });
-    });
-  }
-
-  calculateScalingFactors (model: IModel) {
-    const diagramWindow = document.querySelector('#diagram-window')!;
-
-    const canvasWidth = diagramWindow.clientWidth;
-    const canvasHeight = diagramWindow.clientHeight;
-
-    const leftX = Math.min(...model.entities.map(entity => entity.location.topLeft.x));
-    const rightX = Math.max(...model.entities.map(entity => entity.location.bottomRight.x));
-
-    const topY = Math.max(...model.entities.map(entity => entity.location.topLeft.y));
-    const bottomY = Math.min(...model.entities.map(entity => entity.location.bottomRight.y));
-
-    const importedWidth = leftX < 0
-      ? (leftX * -1 + rightX)
-      : rightX - leftX;
-
-    const importedHeight = bottomY < 0
-      ? (bottomY * -1 + topY)
-      : topY - bottomY;
-
-    return [canvasWidth / importedWidth, canvasHeight / importedHeight, leftX, topY];
-  }
-
-  calculateLengthBetweenXCoordinates (number: number, leftX: number) {
-    if (number < 0) {
-      return number - leftX;
+        return Math.abs(leftX) + number;
     }
 
-    return Math.abs(leftX) + number;
-  }
+    calculateLengthBetweenYCoordinates(number: number, topY: number) {
+        if (number < 0) {
+            return topY + Math.abs(number);
+        }
 
-  calculateLengthBetweenYCoordinates (number: number, topY: number) {
-    if (number < 0) {
-      return topY + Math.abs(number);
+        return topY - number;
     }
 
-    return topY - number;
-  }
+    render() {
+        const {model} = this.props;
 
-  render () {
-    const {model} = this.props;
+        const [widthScaleFactor, heightScaleFactor, leftX, topY] = this.calculateScalingFactors(model);
 
-    const [widthScaleFactor, heightScaleFactor, leftX, topY] = this.calculateScalingFactors(model);
+        return (
+            <div id="diagramContainer" className='editor relative'>
+                {model.entities.map(entity => {
+                    return <div className="entity absolute bg-white shadow-md" id={entity.id} style={
+                        {
+                            top: this.calculateLengthBetweenYCoordinates(entity.location.topLeft.y, topY) * heightScaleFactor,
+                            left: this.calculateLengthBetweenXCoordinates(entity.location.topLeft.x, leftX) * widthScaleFactor,
+                        }
+                    }>
+                        <div className='p-2 border-b border-grey-lighter font-bold text-grey-darker'>
+                            <p>{entity.name}</p>
+                        </div>
+                        <div className='p-2'>
+                            <table className='text-sm'>
+                                <tbody>
+                                {entity.attributeIds.map(attributeId => {
+                                    const isPrimaryIdentifier = model.attributes[attributeId].domainId !== undefined && model.domains[model.attributes[attributeId].domainId!].dataType === 'I';
 
-    return (
-      <div id="diagramContainer" className='dynamic-demo relative'>
-        {model.entities.map(entity => {
-          return <div className="window" id={entity.id} style={
-            {
-              top: this.calculateLengthBetweenYCoordinates(entity.location.topLeft.y, topY) * heightScaleFactor,
-              left: this.calculateLengthBetweenXCoordinates(entity.location.topLeft.x, leftX) * widthScaleFactor,
-            }
-          }>
-            <p>{entity.name}</p>
-            <table className='text-sm'>
-              {entity.attributeIds.map(attributeId => {
-                return <tr><td>{model.attributes[attributeId].name}</td><td>PK</td><td>DOMAIN</td></tr>
-              })}
-            </table>
-          </div>;
-        })}
-      </div>
-    );
-  }
+                                    return <tr className={isPrimaryIdentifier ? 'primary-identifier-row' : ''}>
+                                        <td className='pr-2'>{model.attributes[attributeId].name}</td>
+                                        <td>{isPrimaryIdentifier ? '<pi>' : ''}</td>
+                                        <td className='pl-2'>{model.attributes[attributeId].domainId === undefined ? model.attributes[attributeId].name : model.domains[model.attributes[attributeId].domainId!].name}</td>
+                                    </tr>
+                                })}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                })}
+            </div>
+        );
+    }
 }
 
 export default Diagram;
---
+++ src/helpers/EndpointFactory.ts
@@ -0,0 +1,79 @@
+import IModel from "../models/IModel";
+import {EndpointOptions} from "jsplumb";
+
+class EndpointFactory {
+    static getAnchorPoints() {
+        return [
+            [0.2, 0, 0, 0, 0, 0],
+            [0.5, 0, 0, 0, 0, 0],
+            [0.8, 0, 0, 0, 0, 0],
+
+            [0, 0.2, 0, 0, 0, 0],
+            [0, 0.5, 0, 0, 0, 0],
+            [0, 0.8, 0, 0, 0, 0],
+
+            [1, 0.2, 0, 0, 0, 0],
+            [1, 0.5, 0, 0, 0, 0],
+            [1, 0.8, 0, 0, 0, 0],
+
+            [0.2, 1, 0, 0, 0, 0],
+            [0.5, 1, 0, 0, 0, 0],
+            [0.8, 1, 0, 0, 0, 0]
+        ];
+    }
+
+    private static buildCardinalityTag(component: any, model: IModel, useFrom: boolean) {
+        const {sourceId, targetId} = component;
+        const relation = model.relations.find(x => x.from.ref === sourceId && x.to.ref === targetId);
+
+        if (relation === undefined) {
+            return null;
+        }
+
+        const elem = document.createElement('div');
+        elem.classList.add('tag', 'z-10');
+        elem.innerHTML = `<p>${useFrom ? relation.from.cardinality : relation.to.cardinality}</p>`;
+        return elem;
+    }
+
+    static create(model: IModel): any {
+        const lineColor = '#30364c';
+        const connector = ['Bezier', {cssClass: 'connectorClass', hoverClass: 'connectorHoverClass'}]
+        const connectorStyle = {
+            strokeWidth: 3,
+            stroke: lineColor
+        };
+        const hoverStyle = {
+            stroke: '#449999'
+        };
+        const overlays = [
+            ['Custom', {
+                create: (component: any) => this.buildCardinalityTag(component, model, true),
+                location: 0.1,
+                id: 'fromCardinalityOverlay'
+            }],
+            ['Custom', {
+                create: (component: any) => this.buildCardinalityTag(component, model, false),
+                location: 0.9,
+                id: 'toCardinalityOverlay'
+            }]
+        ];
+        const endpoint = ['Dot', {cssClass: 'endpointClass', radius: 5, hoverClass: 'endpointHoverClass'}]
+        const endpointStyle = {fill: lineColor};
+
+        return {
+            endpoint: endpoint,
+            paintStyle: endpointStyle,
+            hoverPaintStyle: {fill: '#449999'},
+            isSource: true,
+            isTarget: true,
+            maxConnections: -1,
+            connector: connector,
+            connectorStyle: connectorStyle,
+            connectorHoverStyle: hoverStyle,
+            connectorOverlays: overlays
+        };
+    }
+}
+
+export default EndpointFactory;
